3f93e9d707a24be125fbdfd2f4e18d07b811881c
AlexKukushkinDev <74536600+AlexKukushkinDev@users.noreply.github.com>
2021-08-23 13:55:15 +0300

    Add files via upload

---
+++ todo-app/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, task, target_date, done)
+values(10001, 'alex_kukushkin', 'JPA project', sysdate(), false);
+
+insert into todo(id, username, task, target_date, done)
+values(10002, 'alex_kukushkin', 'Integrate JWT', sysdate(), false);
+
+insert into todo(id, username, task, target_date, done)
+values(10003, 'alex_kukushkin', 'UI improvements', sysdate(), false);
